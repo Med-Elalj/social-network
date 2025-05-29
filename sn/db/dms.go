6e128df7430ea49113399f87e27baabdea050ee2
Mohammed Elalj <MohammedElalj@outlook.com>
2025-05-29 17:45:14 +0100

    ws

---
+++ sn/db/dms.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"fmt"
+
+	"social-network/server/logs"
+)
+
+func AddDm(sUname, rUname int, msg string) error {
+	result, err := DB.Exec(`
+INSERT INTO
+message (sender_id, receiver_id, content)
+VALUES (?, ?, ?);
+`, sUname, rUname, msg)
+	if err != nil {
+		logs.Println("Database insertion error:", err)
+		return fmt.Errorf("could not Save in database")
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil || rowsAffected == 0 {
+		logs.Errorf("Failed to insert message: %v, Rows affected: %d", err, rowsAffected)
+		return fmt.Errorf("could not Save in database")
+	}
+	return nil
+}

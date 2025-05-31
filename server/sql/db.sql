47120fe001f4f8a5c07832ad18c3bd81db5d26db
Med-Elalj <mohammedelalj@outlook.com>
2025-05-31 23:53:24 +0100

    new and improuved db

--- server/sql/db.sql
+++ server/sql/db.sql
@@ -25,7 +25,7 @@ CREATE TABLE IF NOT EXISTS "group" (
   "id" INTEGER PRIMARY KEY,
   "creator_id" INTEGER NOT NULL,
   FOREIGN KEY ("id") REFERENCES "profile" ("id") ON DELETE CASCADE,
-  FOREIGN KEY ("creator_id") REFERENCES "profile" ("id") ON DELETE CASCADE
+  FOREIGN KEY ("creator_id") REFERENCES "person" ("id") ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS "posts" (
@@ -71,72 +71,21 @@ CREATE TABLE IF NOT EXISTS "post_interactions" (
   FOREIGN KEY ("user_id") REFERENCES "person" ("id") ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "group_events" (
-  "id" INTEGER PRIMARY KEY,
-  "group_id" INTEGER NOT NULL,
-  "title" TEXT NOT NULL,
-  "description" TEXT NOT NULL,
-  "event_date" DATETIME NOT NULL,
-  "creator_id" INTEGER NOT NULL,
-  "created_at" TIMESTAMP DEFAULT (CURRENT_TIMESTAMP),
-  "options" TEXT DEFAULT '["Going","Not Going"]',
-  FOREIGN KEY ("creator_id") REFERENCES "person" ("id") ON DELETE CASCADE,
-  FOREIGN KEY ("group_id") REFERENCES "group" ("id") ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS "eventResponse" (
-  "event_id" INTEGER NOT NULL,
-  "user_id" INTEGER NOT NULL,
-  "response" INTEGER NOT NULL,
-  "created_at" DATETIME DEFAULT (CURRENT_TIMESTAMP),
-  PRIMARY KEY ("event_id", "user_id"),
-  FOREIGN KEY ("event_id") REFERENCES "group_events" ("id") ON DELETE CASCADE,
-  FOREIGN KEY ("user_id") REFERENCES "person" ("id") ON DELETE CASCADE
-);
-
 CREATE TABLE IF NOT EXISTS "message" (
   "sender_id" INTEGER NOT NULL,
   "receiver_id" INTEGER NOT NULL,
   "isread" BOOLEAN DEFAULT 0,
-  "content" TEXT NOT NULL,
+  "content" TEXT NOT NULL, -- TODO: in case of notification content should be JSON.
   "created_at" DATETIME NOT NULL DEFAULT (CURRENT_TIMESTAMP),
   PRIMARY KEY ("sender_id", "receiver_id", "created_at"),
   FOREIGN KEY ("sender_id") REFERENCES "person" ("id") ON DELETE CASCADE,
   FOREIGN KEY ("receiver_id") REFERENCES "profile" ("id") ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS "notifications" (
-  "id" INTEGER PRIMARY KEY,
-  "type" TEXT NOT NULL,
-  "related_id" INTEGER,
-  "receiver_id" INTEGER NOT NULL,
-  "sender_id" INTEGER NOT NULL,
-  "is_read" BOOLEAN DEFAULT 0,
-  "created_at" DATETIME DEFAULT (CURRENT_TIMESTAMP),
-  FOREIGN KEY ("receiver_id") REFERENCES "person" ("id") ON DELETE CASCADE,
-  FOREIGN KEY ("sender_id") REFERENCES "profile" ("id") ON DELETE CASCADE
-);
-
--- add person transaction
--- BEGIN TRANSACTION;
-
--- Step 1: Insert into profile
--- INSERT INTO profile (display_name, avatar, description, is_public, is_person)
--- VALUES ('jdoe', null, 'Just a sample user', 1, 1);
-
--- -- Step 2: Insert into person using last inserted profile id
--- INSERT INTO person (ent, email, first_name, last_name, password_hash, date_of_birth)
--- VALUES (last_insert_rowid(), 'jdoe@example.com', 'John', 'Doe', 'hashed_password_here', '1990-01-01');
-
--- COMMIT;
-
--- -- add group transaction
--- BEGIN TRANSACTION;
-
--- INSERT INTO "profile" (display_name, avatar, description, is_public, is_person)
--- VALUES ('GroupName', null, 'Just a sample group', 1, 0);
-
--- INSERT INTO "group" (id, creator_id)
--- VALUES (last_insert_rowid(), 'id_of_creator_profile');
-
--- COMMIT;
+CREATE TRIGGER IF NOT EXISTS check_event_post_group
+BEFORE INSERT ON posts
+FOR EACH ROW
+WHEN NEW.title = '<EVENT>' AND NEW.group_id IS NULL
+BEGIN
+  SELECT RAISE(ABORT, 'Event posts must have a group_id');
+END;

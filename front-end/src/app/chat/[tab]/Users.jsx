f3cb2366c0a4ab37fee60b383bded460a2abee22
Med-Elalj <mohammedElalj@outlook.com>
2025-06-27 10:52:35 +0100

    mrhioui : working in messages page

---
+++ front-end/src/app/chat/[tab]/Users.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import Style from "../chat.module.css";
+import Image from "next/image";
+
+export default function Users({ onUserSelect }) {
+    const [activeIndex, setActiveIndex] = useState(null);
+
+    const users = [
+        { avatar: "iconMale.png", name: "User 1", message: "Lorem ipsum, dolor sit amet consectetu", status: "online" },
+        { avatar: "iconFemale.png", name: "User 2", message: "Lorem ipsum, dolor sit amet consectetu", status: "online" },
+        { avatar: "iconMale.png", name: "User 3", message: "Lorem ipsum, dolor sit amet consectetu", status: "offline" },
+    ];
+    const handleUserClick = (user, index) => {
+        setActiveIndex(index);
+        onUserSelect(user);
+    };
+
+    return (
+        <div className={Style.users}>
+            {users && users.length > 0 ? (
+                <>
+                    {users.map((user, index) => (
+                        <div
+                            key={index}
+                            className={`${Style.user} ${activeIndex === index ? Style.active : ""}`}
+                            onClick={() => handleUserClick(user, index)}
+                        >
+                            <div className={Style.userImageWrapper}>
+                                <Image
+                                    src={`/${user.avatar ?? "iconMale.png"}`}
+                                    width={50}
+                                    height={50}
+                                    alt="userProfile"
+                                />
+                                {activeIndex === index && <span className={Style.activeIndicator} />}
+                            </div>
+                            <div>
+                                <h4>{user.name}</h4>
+                                <p>{user.message}</p>
+                            </div>
+                            {activeIndex === index && (
+                                <Image className={Style.last} src="/iconMale.png" width={20} height={20} alt="userProfile" />
+                            )}
+                        </div>
+                    ))}
+                </>
+            ) : (
+                <>
+                    <div className={Style.message}>
+                        <h4>No messages</h4>
+                        <h5>When you have chats, you’ll see them here.</h5>
+                    </div>
+                </>
+            )
+            }
+        </div >
+    );
+}

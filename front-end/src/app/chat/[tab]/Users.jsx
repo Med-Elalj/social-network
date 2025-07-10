9e5177a9bfd00cbb9419a8a10e85d722ea317e94
mmihit <med86004@gmail.com>
2025-07-10 11:27:37 +0100

    waiting notifs and avatar

--- front-end/src/app/chat/[tab]/Users.jsx
+++ front-end/src/app/chat/[tab]/Users.jsx
@@ -1,9 +1,26 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Style from "../chat.module.css";
 import Image from "next/image";
+import { useSearchParams } from 'next/navigation';
+
 
 export default function Users({ users, onUserSelect }) {
     const [activeIndex, setActiveIndex] = useState(null);
+    const searchParams = useSearchParams();
+    const goTo = searchParams.get('goTo');
+
+
+    useEffect(() => {
+        if (goTo && users?.length > 0) {
+
+            users.forEach((element, idx) => {
+                if (element.name == goTo) {
+                    setActiveIndex(idx);
+                    onUserSelect(element);
+                }
+            });
+        }
+    }, [users]);
 
     const handleUserClick = (user, index) => {
         console.log("click", user)
